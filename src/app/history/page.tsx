4ef1cb7fe1d8c57ea202ec7e2b8723d7a4e92caf
sananbsd2021 <89647979+sananbsd2021@users.noreply.github.com>
2024-12-29 16:03:54 +0700

    Update page.tsx

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState, useEffect } from 'react';
 
 interface History {
@@ -21,7 +22,7 @@ async function fetchHistories(): Promise<History[]> {
 
 export default function HistoriesListPage() {
   const [historiesList, setHistoriesList] = useState<History[]>([]);
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
 
   useEffect(() => {
@@ -31,68 +32,68 @@ export default function HistoriesListPage() {
         setHistoriesList(histories);
       } catch (err) {
         setError(err instanceof Error ? err.message : 'An unknown error occurred.');
-      } finally {
-        setLoading(false);
+      // } finally {
+      //   setLoading(false);
       }
     };
 
     getHistories();
   }, []);
 
-  if (loading) {
-    return (
-      <div className="container mx-auto p-4">
-        <h1
-          className="rounded text-xl font-bold mb-6 bg-blue-600 p-2 mx-auto flex justify-center text-white"
-          role="status"
-        >
-          Loading...
-        </h1>
-      </div>
-    );
-  }
+  // if (loading) {
+  //   return (
+  //     <div className="container mx-auto p-4">
+  //       <h1
+  //         className="rounded text-xl font-bold mb-6 bg-blue-600 p-2 mx-auto flex justify-center text-white"
+  //         role="status"
+  //       >
+  //         Loading...
+  //       </h1>
+  //     </div>
+  //   );
+  // }
 
-  if (error) {
-    return (
-      <div className="container mx-auto p-4">
-        <h1
-          className="rounded text-xl font-bold mb-6 bg-red-600 p-2 mx-auto flex justify-center text-white"
-          role="alert"
-        >
-          {error}
-        </h1>
-      </div>
-    );
-  }
+  // if (error) {
+  //   return (
+  //     <div className="container mx-auto p-4">
+  //       <h1
+  //         className="rounded text-xl font-bold mb-6 bg-red-600 p-2 mx-auto flex justify-center text-white"
+  //         role="alert"
+  //       >
+  //         {error}
+  //       </h1>
+  //     </div>
+  //   );
+  // }
 
-  if (historiesList.length === 0) {
-    return (
-      <div className="container mx-auto p-4">
-        <h1
-          className="rounded text-xl font-bold mb-6 bg-yellow-600 p-2 mx-auto flex justify-center text-white"
-          role="alert"
-        >
-          No histories available
-        </h1>
-      </div>
-    );
-  }
+  // if (historiesList.length === 0) {
+  //   return (
+  //     <div className="container mx-auto p-4">
+  //       <h1
+  //         className="rounded text-xl font-bold mb-6 bg-yellow-600 p-2 mx-auto flex justify-center text-white"
+  //         role="alert"
+  //       >
+  //         No histories available
+  //       </h1>
+  //     </div>
+  //   );
+  // }
 
   return (
-    <div className="container mx-auto p-4">
+    <div className="mx-auto p-4">
       <h1
         className="rounded text-xl font-bold mb-6 bg-blue-600 p-2 mx-auto flex justify-center text-white"
         aria-label="School Histories"
       >
         ประวัติโรงเรียน
       </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid gap-6">
         {historiesList.map((history) => (
           <div
             key={history._id}
-            className="bg-white shadow-md rounded-lg p-6 border border-gray-200"
+            className="bg-gray-100 hover:bg-gray-200 border border-gray-300 shadow-md rounded-lg p-6"
           >
-            <h2 className="text-lg font-bold mt-4">{history.title}</h2>
+            {/* <h2 className="text-3xl font-bold mt-4 text-center">{history.title}</h2> */}
             <p className="text-gray-600 mb-4">{history.description}</p>
           </div>
         ))}
